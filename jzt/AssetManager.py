be0237bb45bac8472c646a71787a470ec270f315
talhabinijaz576 <talhabinijaz576@yahoo.com>
2021-01-15 16:37:13 +0000

    Add files via upload

--- jzt/AssetManager.py
+++ jzt/AssetManager.py
@@ -1,21 +1,40 @@
 import requests
 import datetime
 import ast
+import os
 
 
 class AssetManager:
     
     url="https://console.jazeetech.com/getAsset/"
+    token_file = str(os.path.join(os.getenv('PROGRAMDATA'), "jazee_orchestrator", "token.txt"))
+    token = None
+    access_key=None
     
-    def __init__(self, access_key, url=""):
+    def __init__(self, access_key=None, token_file=None, url=""):
         
-        self.access_key = access_key
+        if(access_key!=None):
+            self.access_key = access_key
+        if(token_file!=None):
+            self.token_file = token_file
         if(len(url)!=0):
             self.url = url
-
+         
+        if(os.path.exists(self.token_file)):
+            self.token = open(self.token_file, 'r').readlines()[0].strip()
+        if(len(self.token)<10):
+            self.token = None
+            
+        if(self.token==None and self.access_key==None):
+            raise Exception("Please provide access key for unregistered device")
+            
+            
     def getAsset(self, name=""):
-           
-        request_url = self.url+"?asset={0}&key={1}".format(name, self.access_key)
+
+        if(self.access_key!=None):
+            request_url = self.url+"?asset={0}&key={1}".format(name, self.access_key)
+        elif(self.token!=None):
+            request_url = self.url+"?asset={0}&token={1}".format(name, self.token)
         response = requests.get(request_url)
         if(response.status_code==200):
             response = response.content.decode('ascii')
@@ -49,6 +68,8 @@ class AssetManager:
         else:
             message = "Connection could not be established. Please check your internet connection."
             raise Exception(message)
+            
+            
     
 
 
