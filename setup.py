43e2c91e5ef732b9a08c7d123adaa6c8fbda7d6c
talhabinijaz576 <talhabinijaz576@yahoo.com>
2021-02-01 18:58:47 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'jzt',
   packages = ['jzt'],
-  version = '1.0.0.2',
+  version = '1.0.0.3',
   license='MIT',
   description = 'JazeeTech Bot Manager',
   author = 'Talha Ijaz',
   author_email = 'talhabinijaz576@yahoo.com',
   url = 'https://github.com/talhabinijaz576/jzt',
-  download_url = 'https://github.com/talhabinijaz576/jzt/archive/1.0.0.2.tar.gz',
+  download_url = 'https://github.com/talhabinijaz576/jzt/archive/1.0.0.3.tar.gz',
   keywords = ['jzt', 'jazee tech', 'jazee', 'commandcenter', 'bot manager', 'asset manager', 'assetmanager'],
   install_requires=[
           'requests',
